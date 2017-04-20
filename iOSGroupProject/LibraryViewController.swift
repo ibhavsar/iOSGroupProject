ec560052b89f255e1bfd0896b487f411d9fcd8b2
Student <Student@zpr-l0007071.local>
2017-04-20 08:56:32 -0400

    Completed hamburger menu. still need to add hamburger menu icon. right now its just a label, and blank views. All the labels in the hamburger menu works.

---
+++ iOSGroupProject/LibraryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LibraryViewController.swift
+//  iOSGroupProject
+//
+//  Created by Student on 2017-04-19.
+//  Copyright © 2017 Ishan Student. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+   
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
