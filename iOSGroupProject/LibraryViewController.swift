225cffc057cbc030d7244c561286fff0042038bf
Johns5949 <johns5949@googleapps.wrdsb.ca>
2017-04-26 08:49:46 -0400

    Fixed warnings of using string literal by converting to selectors.

--- iOSGroupProject/LibraryViewController.swift
+++ iOSGroupProject/LibraryViewController.swift
@@ -17,7 +17,7 @@ class LibraryViewController: UIViewController {
         
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
-            menuButton.action = "revealToggle:"
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
 
