132a2e2589b4642c6821dd6d715a0681f340f14b
a22munir <Student@zpr-l0007069.local>
2017-06-14 22:20:42 -0400

    first 3 working, progress bar not done yet

---
+++ iOSGroupProject/AchieveCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AchieveCollectionViewCell.swift
+//  iOSGroupProject
+//
+//  Created by Student on 2017-06-11.
+//  Copyright © 2017 Ishan Student. All rights reserved.
+//
+
+import UIKit
+
+class AchieveCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var AchieveImageView: UIImageView!
+    
+}
